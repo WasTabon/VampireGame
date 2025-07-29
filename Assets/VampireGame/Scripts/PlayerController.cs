7b77f9b774c0e534fdec56194adb28c1c65f94c2
WasTabon <titanium.jen@gmail.com>
2025-07-29 15:40:04 +0300

    animation

--- Assets/VampireGame/Scripts/PlayerController.cs
+++ Assets/VampireGame/Scripts/PlayerController.cs
@@ -1,5 +1,4 @@
 using UnityEngine;
-using UnityEngine.Serialization;
 
 public class PlayerController : MonoBehaviour
 {
@@ -10,19 +9,34 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private Joystick _joystick;
 
     [SerializeField] private Rigidbody _rigidbody;
+    [SerializeField] private Animator _animator;
+    
+    private bool _isWalking = false;
     
     private void FixedUpdate()
     {
         HandleMovement();
     }
 
+    public void HandleAttack()
+    {
+        _animator.SetTrigger("Attack");
+    }
+    
     private void HandleMovement()
     {
         Vector3 direction = new Vector3(_joystick.Horizontal, 0, _joystick.Vertical);
-        
         _rigidbody.velocity = direction.normalized * _moveSpeed;
+
+        bool hasMovement = direction.sqrMagnitude > 0.01f;
         
-        if (direction != Vector3.zero)
+        if (hasMovement != _isWalking)
+        {
+            _isWalking = hasMovement;
+            _animator.SetBool("Walk", _isWalking);
+        }
+
+        if (hasMovement)
         {
             Quaternion targetRotation = Quaternion.LookRotation(direction, Vector3.up);
             _rigidbody.MoveRotation(Quaternion.Lerp(_rigidbody.rotation, targetRotation, 10f * Time.deltaTime));
