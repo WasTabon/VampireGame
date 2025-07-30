df7bcd363714ec72cd3a49552731bc0ce7a07990
WasTabon <titanium.jen@gmail.com>
2025-07-30 21:43:27 +0300

    level

--- Assets/VampireGame/Scripts/PlayerDetector.cs
+++ Assets/VampireGame/Scripts/PlayerDetector.cs
@@ -3,12 +3,26 @@ using UnityEngine;
 public class PlayerDetector : MonoBehaviour
 {
     [SerializeField] private EnemyController _enemyController;
+    [SerializeField] private LayerMask levelLayerMask;
 
     private void OnTriggerEnter(Collider coll)
     {
         if (coll.gameObject.TryGetComponent(out PlayerTriggerController _))
         {
-            UIController.Instance.ShowLostPanel();
+            Transform playerTransform = coll.transform;
+            Vector3 origin = transform.position;
+            Vector3 direction = (playerTransform.position - origin).normalized;
+            float distance = Vector3.Distance(origin, playerTransform.position);
+
+            // Проверка на наличие объектов слоя "Level" между врагом и игроком
+            if (!Physics.Raycast(origin, direction, distance, levelLayerMask))
+            {
+                UIController.Instance.ShowLostPanel();
+            }
+
+#if UNITY_EDITOR
+            Debug.DrawLine(origin, playerTransform.position, Color.red, 1f);
+#endif
         }
     }
 }
