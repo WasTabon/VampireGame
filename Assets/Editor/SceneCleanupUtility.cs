57c013d08890cb33e0658e468edf1bb25031c05e
WasTabon <titanium.jen@gmail.com>
2025-07-31 17:31:19 +0300

    scene

--- Assets/Editor/SceneCleanupUtility.cs
+++ Assets/Editor/SceneCleanupUtility.cs
@@ -1,18 +1,52 @@
-using System.Collections;
-using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using UnityEditor.SceneManagement;
 
 public class SceneCleanupUtility : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [MenuItem("Tools/Cleanup Scene/Remove Colliders & Set Static & Mixed Lights")]
+    public static void CleanScene()
     {
-        
-    }
+        if (!EditorUtility.DisplayDialog("Cleanup Scene", "Это действие необратимо. Продолжить?", "Да", "Отмена"))
+            return;
 
-    // Update is called once per frame
-    void Update()
-    {
+        int removedColliders = 0;
+        int staticObjects = 0;
+        int mixedLights = 0;
         
+        foreach (Collider collider in GameObject.FindObjectsOfType<Collider>())
+        {
+            GameObject obj = collider.gameObject;
+            Undo.DestroyObjectImmediate(collider);
+            removedColliders++;
+        }
+        
+        foreach (MeshRenderer renderer in GameObject.FindObjectsOfType<MeshRenderer>())
+        {
+            GameObject obj = renderer.gameObject;
+            GameObjectUtility.SetStaticEditorFlags(obj, StaticEditorFlags.ContributeGI |
+                                                        StaticEditorFlags.BatchingStatic |
+                                                        StaticEditorFlags.NavigationStatic |
+                                                        StaticEditorFlags.OccludeeStatic |
+                                                        StaticEditorFlags.OccluderStatic |
+                                                        StaticEditorFlags.OffMeshLinkGeneration |
+                                                        StaticEditorFlags.ReflectionProbeStatic |
+                                                        StaticEditorFlags.ContributeGI);
+            staticObjects++;
+        }
+        
+        foreach (Light light in GameObject.FindObjectsOfType<Light>())
+        {
+            if (light.lightmapBakeType != LightmapBakeType.Mixed)
+            {
+                Undo.RecordObject(light, "Set Light to Mixed");
+                light.lightmapBakeType = LightmapBakeType.Mixed;
+                mixedLights++;
+            }
+        }
+
+        EditorSceneManager.MarkSceneDirty(EditorSceneManager.GetActiveScene());
+
+        Debug.Log($"✅ Завершено:\nУдалено коллайдеров: {removedColliders}\nStatic назначено: {staticObjects}\nСветов переведено в Mixed: {mixedLights}");
     }
 }
