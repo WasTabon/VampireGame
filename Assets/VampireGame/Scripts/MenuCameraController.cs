b750592bb7b4d006b0cdfa92b502b090c567bf06
WasTabon <titanium.jen@gmail.com>
2025-07-31 18:23:52 +0300

    levels

---
+++ Assets/VampireGame/Scripts/MenuCameraController.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using Cinemachine;
+
+public class MenuCameraController : MonoBehaviour
+{
+    [SerializeField] private CinemachineVirtualCamera mainCam;
+    [SerializeField] private CinemachineVirtualCamera settingsCam;
+    [SerializeField] private CinemachineVirtualCamera shopCam;
+
+    [ContextMenu("Go To Main Camera")]
+    public void GoToMain()
+    {
+        SetActiveCamera(mainCam);
+    }
+
+    [ContextMenu("Go To Settings Camera")]
+    public void GoToSettings()
+    {
+        SetActiveCamera(settingsCam);
+    }
+
+    [ContextMenu("Go To Shop Camera")]
+    public void GoToShop()
+    {
+        SetActiveCamera(shopCam);
+    }
+
+    private void SetActiveCamera(CinemachineVirtualCamera targetCam)
+    {
+        mainCam.Priority = 0;
+        settingsCam.Priority = 0;
+        shopCam.Priority = 0;
+
+        targetCam.Priority = 10;
+    }
+}
